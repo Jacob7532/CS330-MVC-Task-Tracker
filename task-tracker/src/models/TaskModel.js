fb46f3b51cda5a04cb353a87465079e88cb4ac35
Jacob <66694749+Jacob7532@users.noreply.github.com>
2023-12-11 15:10:39 -0800

    fixed a variable which was incorrectly named

--- task-tracker/src/models/TaskModel.js
+++ task-tracker/src/models/TaskModel.js
@@ -1,7 +1,7 @@
 // /src/models/TaskModel.js
 class Task {
     constructor(name, status, urgency, startDate, endDate) {
-        this.name = name;
+        this.taskName = name;
         this.status = status;
         this.urgency = urgency;
         this.startDate = startDate;
